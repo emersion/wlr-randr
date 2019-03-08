5f933d96fd3836a0e622e933d5201172f05b797e
emersion <contact@emersion.fr>
2019-03-08 16:15:21 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,170 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wayland-client.h>
+#include "wlr-output-management-unstable-v1-client-protocol.h"
+
+struct randr_state;
+
+struct randr_head {
+	struct randr_state *state;
+	struct zwlr_output_head_v1 *wlr_head;
+	struct wl_list link;
+
+	char *name, *description;
+	int32_t phys_width, phys_height;
+
+	bool enabled;
+};
+
+struct randr_state {
+	struct zwlr_output_manager_v1 *output_manager;
+
+	struct wl_list heads;
+	uint32_t serial;
+	bool running;
+};
+
+static void print_state(struct randr_state *state) {
+	struct randr_head *head;
+	wl_list_for_each(head, &state->heads, link) {
+		printf("%s \"%s\"\n", head->name, head->description);
+		if (head->phys_width > 0 && head->phys_height > 0) {
+			printf("  Physical size: %dx%d\n",
+				head->phys_width, head->phys_height);
+		}
+		printf("  Enabled: %s\n", head->enabled ? "yes" : "no");
+	}
+}
+
+static void head_handle_name(void *data,
+		struct zwlr_output_head_v1 *wlr_head, const char *name) {
+	struct randr_head *head = data;
+	head->name = strdup(name);
+}
+
+static void head_handle_description(void *data,
+		struct zwlr_output_head_v1 *wlr_head, const char *description) {
+	struct randr_head *head = data;
+	head->description = strdup(description);
+}
+
+static void head_handle_physical_size(void *data,
+		struct zwlr_output_head_v1 *wlr_head, int32_t width, int32_t height) {
+	struct randr_head *head = data;
+	head->phys_width = width;
+	head->phys_height = height;
+}
+
+static void head_handle_enabled(void *data,
+		struct zwlr_output_head_v1 *wlr_head, int32_t enabled) {
+	struct randr_head *head = data;
+	head->enabled = !!enabled;
+}
+
+static void head_handle_finished(void *data,
+		struct zwlr_output_head_v1 *wlr_head) {
+	struct randr_head *head = data;
+	wl_list_remove(&head->link);
+	free(head->name);
+	free(head->description);
+	free(head);
+}
+
+static const struct zwlr_output_head_v1_listener head_listener = {
+	.name = head_handle_name,
+	.description = head_handle_description,
+	.physical_size = head_handle_physical_size,
+	.enabled = head_handle_enabled,
+	.finished = head_handle_finished,
+};
+
+static void output_manager_handle_head(void *data,
+		struct zwlr_output_manager_v1 *manager,
+		struct zwlr_output_head_v1 *wlr_head) {
+	struct randr_state *state = data;
+
+	struct randr_head *head = calloc(1, sizeof(*head));
+	head->state = state;
+	head->wlr_head = wlr_head;
+	wl_list_insert(&state->heads, &head->link);
+
+	zwlr_output_head_v1_add_listener(wlr_head, &head_listener, head);
+}
+
+static void output_manager_handle_done(void *data,
+		struct zwlr_output_manager_v1 *manager, uint32_t serial) {
+	struct randr_state *state = data;
+	state->serial = serial;
+
+	print_state(state);
+	state->running = false;
+}
+
+static void output_manager_handle_finished(void *data,
+		struct zwlr_output_manager_v1 *manager) {
+	// This space is intentionally left blank
+}
+
+static const struct zwlr_output_manager_v1_listener output_manager_listener = {
+	.head = output_manager_handle_head,
+	.done = output_manager_handle_done,
+	.finished = output_manager_handle_finished,
+};
+
+static void registry_handle_global(void *data, struct wl_registry *registry,
+		uint32_t name, const char *interface, uint32_t version) {
+	struct randr_state *state = data;
+
+	if (strcmp(interface, zwlr_output_manager_v1_interface.name) == 0) {
+		state->output_manager = wl_registry_bind(registry, name,
+			&zwlr_output_manager_v1_interface, 1);
+		zwlr_output_manager_v1_add_listener(state->output_manager,
+			&output_manager_listener, state);
+	}
+}
+
+static void registry_handle_global_remove(void *data,
+		struct wl_registry *registry, uint32_t name) {
+	// This space is intentionally left blank
+}
+
+static const struct wl_registry_listener registry_listener = {
+	.global = registry_handle_global,
+	.global_remove = registry_handle_global_remove,
+};
+
+int main(int argc, char *argv[]) {
+	struct randr_state state = { .running = true };
+	wl_list_init(&state.heads);
+
+	struct wl_display *display = wl_display_connect(NULL);
+	if (display == NULL) {
+		fprintf(stderr, "failed to connect to display\n");
+		return EXIT_FAILURE;
+	}
+
+	struct wl_registry *registry = wl_display_get_registry(display);
+	wl_registry_add_listener(registry, &registry_listener, &state);
+	wl_display_dispatch(display);
+	wl_display_roundtrip(display);
+
+	if (state.output_manager == NULL) {
+		fprintf(stderr, "compositor doesn't support "
+			"wlr-output-management-unstable-v1\n");
+		return EXIT_FAILURE;
+	}
+
+	while (state.running && wl_display_dispatch(display) != -1) {
+		// This space intentionally left blank
+	}
+
+	// TODO: destroy heads
+	zwlr_output_manager_v1_destroy(state.output_manager);
+	wl_registry_destroy(registry);
+	wl_display_disconnect(display);
+
+	return EXIT_SUCCESS;
+}
